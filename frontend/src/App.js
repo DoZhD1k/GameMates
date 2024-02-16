b600470430c1e6065226d31470c78ea4351efc69
DoZhD1k <chereshnikovarlen03@gmail.com>
2024-02-16 17:57:59 +0600

    changed main files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header/Header";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+import FAQs from "./pages/FAQs";
+import AboutUs from "./pages/AboutUs";
+import EyeTraining from "./pages/EyeTraining";
+import GamePage from "./pages/GamePage";
+import AdminPanel from "./pages/AdminPages/AdminPanel";
+import AdminGames from "./pages/AdminPages/AdminGames";
+import AdminUsers from "./pages/AdminPages/AdminUsers";
+// import Testing from "./components/AddCardModal/Testing";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/eye" element={<EyeTraining />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/faqs" element={<FAQs />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route
+            path="/AdminstratorPanelControll/DashboardHomePage"
+            element={<AdminPanel />}
+          />
+          <Route
+            path="/AdminstratorPanelControll/DashboardUsers"
+            element={<AdminUsers />}
+          />
+          <Route
+            path="/AdminstratorPanelControll/DashboardGames"
+            element={<AdminGames />}
+          />
+          {/* <Route path="/addcard" element={<Testing />} /> */}
+
+          <Route path="/games/:gameId" element={<GamePage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
